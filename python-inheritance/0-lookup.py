bdc3bfd27588a118a601eecfa89506f7e3518d83
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-29 06:37:35 -0700

    fix pep8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Module that defines the lookup function."""
- 
- 
+
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object.
- 
+
     Args:
         obj: any object.
- 
+
     Returns:
         list: sorted list of attributes/methods of obj.
     """
