39a440c6c988e9e7ab39a658f1e7e0b29fa2e83e
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 11:36:35 +0200

    Update 102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me and displays "You got me!"
-curl -s -L "0.0.0.0:5000/catch_me"
+# Send a spoofed request that tricks the server into believing user_id is 98
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me"
