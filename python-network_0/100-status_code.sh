d7628da85003006b6dc1d3478fb872ea500fd7b8
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 02:28:13 -0700

    Adding these too!

---
+++ python-network_0/100-status_code.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Displays only the status code of the response for a given URL
+curl -s -o /dev/null -w "%{http_code}" "$1"
---
+++ python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Makes a request to 0.0.0.0:5000/catch_me and displays "You got me!"
+curl -s -L "0.0.0.0:5000/catch_me"
