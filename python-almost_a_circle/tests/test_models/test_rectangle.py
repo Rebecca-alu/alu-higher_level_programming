3d25701f377f3ebc4be6394ba19f83d4d1a09fda
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 13:42:38 -0700

    completed

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """Unittests for models.rectangle.Rectangle"""
 import unittest
+import os
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
     """Unittests for the Rectangle class"""
+
+    def tearDown(self):
+        """Remove Rectangle.json if it was created during a test"""
+        try:
+            os.remove("Rectangle.json")
+        except IOError:
+            pass
 
     def test_width_height(self):
         r = Rectangle(10, 2)
@@ -22,6 +30,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.x, 3)
         self.assertEqual(r.y, 4)
 
+    def test_rectangle_1_2_3_4_5(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual((r.width, r.height, r.x, r.y, r.id),
+                          (1, 2, 3, 4, 5))
+
     def test_width_not_int(self):
         with self.assertRaises(TypeError):
             Rectangle("10", 2)
@@ -30,21 +43,37 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(10, "2")
 
+    def test_y_not_int(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
     def test_width_negative(self):
         with self.assertRaises(ValueError):
-            Rectangle(-10, 2)
+            Rectangle(-1, 2)
+
+    def test_height_negative(self):
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
 
     def test_width_zero(self):
         with self.assertRaises(ValueError):
             Rectangle(0, 2)
 
-    def test_x_not_int(self):
-        with self.assertRaises(TypeError):
-            Rectangle(10, 2, {}, 0)
+    def test_height_zero(self):
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+
+    def test_x_negative(self):
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
 
     def test_y_negative(self):
         with self.assertRaises(ValueError):
-            Rectangle(10, 2, 0, -1)
+            Rectangle(1, 2, 3, -4)
+
+    def test_x_not_int(self):
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, {}, 0)
 
     def test_area(self):
         r = Rectangle(3, 2)
@@ -53,6 +82,18 @@ class TestRectangle(unittest.TestCase):
     def test_str(self):
         r = Rectangle(4, 6, 2, 1, 12)
         self.assertEqual(str(r), "[Rectangle] (12) 2/1 - 4/6")
+
+    def test_display_without_x_and_y(self):
+        r = Rectangle(2, 2)
+        r.display()
+
+    def test_display_without_y(self):
+        r = Rectangle(2, 2, 1)
+        r.display()
+
+    def test_display(self):
+        r = Rectangle(2, 2, 1, 1)
+        r.display()
 
     def test_update_args(self):
         r = Rectangle(10, 10, 10, 10)
@@ -73,6 +114,57 @@ class TestRectangle(unittest.TestCase):
     def test_to_dictionary_type(self):
         r = Rectangle(10, 2, 1, 9, 5)
         self.assertIsInstance(r.to_dictionary(), dict)
+
+    def test_create_id_only(self):
+        r = Rectangle.create(**{'id': 89})
+        self.assertEqual(r.id, 89)
+
+    def test_create_id_width(self):
+        r = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual((r.id, r.width), (89, 1))
+
+    def test_create_id_width_height(self):
+        r = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual((r.id, r.width, r.height), (89, 1, 2))
+
+    def test_create_id_width_height_x(self):
+        r = Rectangle.create(
+            **{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual((r.id, r.width, r.height, r.x), (89, 1, 2, 3))
+
+    def test_create_id_width_height_x_y(self):
+        r = Rectangle.create(
+            **{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual((r.id, r.width, r.height, r.x, r.y),
+                          (89, 1, 2, 3, 4))
+
+    def test_save_to_file_none(self):
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_to_file_empty(self):
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_to_file_one(self):
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        self.assertTrue(os.path.exists("Rectangle.json"))
+
+    def test_load_from_file_no_file(self):
+        try:
+            os.remove("Rectangle.json")
+        except IOError:
+            pass
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+    def test_load_from_file_exists(self):
+        Rectangle.save_to_file([Rectangle(1, 2), Rectangle(3, 4)])
+        result = Rectangle.load_from_file()
+        self.assertEqual(len(result), 2)
+        for r in result:
+            self.assertIsInstance(r, Rectangle)
 
 
 if __name__ == "__main__":
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """Unittests for models.square.Square"""
 import unittest
+import os
 from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
     """Unittests for the Square class"""
+
+    def tearDown(self):
+        """Remove Square.json if it was created during a test"""
+        try:
+            os.remove("Square.json")
+        except IOError:
+            pass
 
     def test_size(self):
         s = Square(5)
@@ -16,6 +24,51 @@ class TestSquare(unittest.TestCase):
         s = Square(5)
         self.assertEqual(s.x, 0)
         self.assertEqual(s.y, 0)
+
+    def test_square_1(self):
+        s = Square(1)
+        self.assertEqual((s.width, s.height, s.x, s.y), (1, 1, 0, 0))
+
+    def test_square_1_2(self):
+        s = Square(1, 2)
+        self.assertEqual((s.width, s.height, s.x, s.y), (1, 1, 2, 0))
+
+    def test_square_1_2_3(self):
+        s = Square(1, 2, 3)
+        self.assertEqual((s.width, s.height, s.x, s.y), (1, 1, 2, 3))
+
+    def test_square_1_2_3_4(self):
+        s = Square(1, 2, 3, 4)
+        self.assertEqual((s.width, s.height, s.x, s.y, s.id),
+                          (1, 1, 2, 3, 4))
+
+    def test_size_not_int(self):
+        with self.assertRaises(TypeError):
+            Square("1")
+
+    def test_x_not_int(self):
+        with self.assertRaises(TypeError):
+            Square(1, "2")
+
+    def test_y_not_int(self):
+        with self.assertRaises(TypeError):
+            Square(1, 2, "3")
+
+    def test_size_negative(self):
+        with self.assertRaises(ValueError):
+            Square(-1)
+
+    def test_x_negative(self):
+        with self.assertRaises(ValueError):
+            Square(1, -2)
+
+    def test_y_negative(self):
+        with self.assertRaises(ValueError):
+            Square(1, 2, -3)
+
+    def test_size_zero(self):
+        with self.assertRaises(ValueError):
+            Square(0)
 
     def test_str(self):
         s = Square(3, 1, 3, 3)
@@ -45,15 +98,69 @@ class TestSquare(unittest.TestCase):
         s.update(1, 2, 3, 4)
         self.assertEqual(str(s), "[Square] (1) 3/4 - 2")
 
-    def test_update_kwargs(self):
+    def test_update_kwargs_size_id(self):
         s = Square(5)
         s.update(size=7, id=89, y=1)
         self.assertEqual(str(s), "[Square] (89) 0/1 - 7")
+
+    def test_update_kwargs_id_size_x(self):
+        s = Square(5)
+        s.update(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual((s.id, s.size, s.x), (89, 1, 2))
+
+    def test_update_kwargs_id_size_x_y(self):
+        s = Square(5)
+        s.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual((s.id, s.size, s.x, s.y), (89, 1, 2, 3))
 
     def test_to_dictionary(self):
         s = Square(10, 2, 1, 1)
         d = s.to_dictionary()
         self.assertEqual(d, {"id": 1, "size": 10, "x": 2, "y": 1})
+
+    def test_create_id_only(self):
+        s = Square.create(**{'id': 89})
+        self.assertEqual(s.id, 89)
+
+    def test_create_id_size(self):
+        s = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual((s.id, s.size), (89, 1))
+
+    def test_create_id_size_x(self):
+        s = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual((s.id, s.size, s.x), (89, 1, 2))
+
+    def test_create_id_size_x_y(self):
+        s = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual((s.id, s.size, s.x, s.y), (89, 1, 2, 3))
+
+    def test_save_to_file_none(self):
+        Square.save_to_file(None)
+        with open("Square.json") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_to_file_empty(self):
+        Square.save_to_file([])
+        with open("Square.json") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_to_file_one(self):
+        Square.save_to_file([Square(1)])
+        self.assertTrue(os.path.exists("Square.json"))
+
+    def test_load_from_file_no_file(self):
+        try:
+            os.remove("Square.json")
+        except IOError:
+            pass
+        self.assertEqual(Square.load_from_file(), [])
+
+    def test_load_from_file_exists(self):
+        Square.save_to_file([Square(1), Square(2, 1, 1)])
+        result = Square.load_from_file()
+        self.assertEqual(len(result), 2)
+        for s in result:
+            self.assertIsInstance(s, Square)
 
 
 if __name__ == "__main__":
