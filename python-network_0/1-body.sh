39b6d45a2c06727ca2cb4c2a6b43f697159850fc
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 11:16:21 +0200

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Sends a GET request and displays the body, only if status code is 200
 curl -s -o /tmp/body_output -w "%{http_code}" "$1" > /tmp/body_status
 if [ "$(cat /tmp/body_status)" = "200" ]; then
