408e9cfa70c7ba4c0273c6865543127eaa2093a5
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 12:05:46 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Fetch https://alu-intranet.hbtn.io/status using the urllib package.
-
-This module fetches the status endpoint and displays the raw body
-of the response along with its type and its utf-8 decoded content.
-"""
+"""Fetches https://intranet.hbtn.io/status using requests."""
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.text))
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
