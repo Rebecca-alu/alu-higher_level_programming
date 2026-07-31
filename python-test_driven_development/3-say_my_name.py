46fcd35e5b976ea6dee26c65757a234c3956762b
Rebecca-alu <r.benegusen@alustudent.com>
2026-07-31 12:41:54 +0200

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Module that prints My name is <first name> <last name>."""
+"""Module that prints a full name.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print My name is <first name> <last name>.
+    """Print 'My name is <first_name> <last_name>'.
 
     Args:
-        first_name (str): The first name.
-        last_name (str): The last name (default "").
+        first_name: string, the first name.
+        last_name: string, the last name, defaults to an empty string.
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
+        TypeError: if first_name is not a string, or if last_name is
+            not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
