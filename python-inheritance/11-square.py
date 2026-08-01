8728eff5706fd86eb7064d5196fe5123675bc969
Rebecca-alu <r.benegusen@alustudent.com>
2026-08-01 22:32:21 +0200

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""Module that defines the Square class."""
+"""Module defining a Square class with custom string representation."""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square, inheriting from Rectangle."""
+    """A square with [Square] <width>/<height> representation."""
 
     def __init__(self, size):
         """Initialize a new Square.
 
         Args:
-            size (int): the size of the square, must be > 0.
+            size: Positive integer size.
         """
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
     def __str__(self):
-        """Return the string representation of the square."""
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                        self._Rectangle__height)
+        """Return the square description: [Square] <width>/<height>."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
