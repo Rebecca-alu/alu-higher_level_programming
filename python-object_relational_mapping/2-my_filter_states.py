28fe69c17a6b8146f9f62a0e2d94db24775d002e
Rebecca-alu <r.benegusen@alustudent.com>
2026-08-01 22:22:35 +0200

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Lists all states matching an argument (vulnerable to SQL injection)"""
-import MySQLdb
+"""Script that takes an argument and displays all values in the states
+table where name matches the argument (vulnerable to SQL injection)."""
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
-        host="localhost", port=3306,
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC" \
-        .format(sys.argv[4])
-    cur.execute(query)
+    cur.execute(
+        "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY id ASC".format(
+            state_name
+        )
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
